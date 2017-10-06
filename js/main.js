01816b5b55ba1e4c6de00136f9329146ca800631
Jameson Rebutoc <jamtravreblo@gmail.com>
2017-10-06 03:32:50 -0700

    Added comments to the js file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
+// a variable that refers to the id of the entire popup div
 var popUp = document.getElementById("popUpId");
 
+// a variable that takes the initial trigger sitemap image
 var takeImage = document.getElementById("imgSiteMapTrigger");
-var takePopUp = document.getElementById("imgSiteMapPopUp");
 
+// on click of the trigger image, set the style to pop-up
 takeImage.onclick = function() {
   popUp.style.display = "block";
 }
 
+// a variable that refers to the class name of the X button
 var closeButton = document.getElementsByClassName("closePopUp")[0];
 
+// on click of the x button, display nothing (remove the pop-up)
 closeButton.onclick = function() {
   popUp.style.display="none";
 }
