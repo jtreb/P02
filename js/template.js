b6591402a8115c830e5d08a478c05bce546e73bb
Unknown <yla312@sfu.ca>
2017-10-12 18:33:37 -0700

    header scrolling update

---
+++ js/template.js
@@ -0,0 +1,13 @@
+
+// changing background-color opacity of header
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($ (this).scrollTop() > 0) {
+      $('.headerBoxDark').css('background-color','rgba(0,0,0,0.9)');
+      $('.headerBoxLight').css('background-color','rgba(255,255,255,0.9)');
+    } else {
+      $('.headerBoxDark').css('background-color','rgba(0,0,0,0)');
+      $('.headerBoxLight').css('background-color','rgba(255,255,255,0.9)');
+    }
+  })
+})
